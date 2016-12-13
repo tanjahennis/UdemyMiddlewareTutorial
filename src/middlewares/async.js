dcc35e18ad0dd0545f6469aad49db5ab1e2008f1
Tanja Hennis <tmhennis@gmail.com>
2016-12-13 14:38:44 +0100

    handling unrelated actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,12 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action)
+    // If action does nog have payload
+    // or, the payload does not have a .then property
+    // we don't care about it, send it on
+    if (!action.payload || !action.payload.then) {
+      return next(action)
+    }
 
-    next(action)
+    console.log('We don\'t have a promise', action)
   };
 }
